49f7da1e77d4992d5a7b158888830af0b1df8118
StatistNo1 <80486109+StatistNo1@users.noreply.github.com>
2023-09-03 01:38:13 +0200

    Initial commit

---
+++ Common/Source/NoLowerPsyfocusLimit/NoLowerPsyfocusLimit/Initializer.cs
@@ -0,0 +1,18 @@
+﻿namespace NoLowerPsyfocusLimit {
+    [Verse.StaticConstructorOnStartup]
+    public static class Initializer {
+        static Initializer() {
+            HarmonyLib.Harmony harmony = new HarmonyLib.Harmony("NoLowerPsyfocusLimit");
+            harmony.PatchAll(System.Reflection.Assembly.GetExecutingAssembly());
+        }
+    }
+    [HarmonyLib.HarmonyPatch(typeof(RimWorld.Pawn_PsychicEntropyTracker))]
+    [HarmonyLib.HarmonyPatch("PsyfocusBand", HarmonyLib.MethodType.Getter)]
+    static class NoLowerPsycocusLimit_Patch {
+        [HarmonyLib.HarmonyPrefix]
+        public static bool PsyfocusBand_Prefix(ref int __result) {
+            __result = RimWorld.Pawn_PsychicEntropyTracker.MaxAbilityLevelPerPsyfocusBand.Count - 1;
+            return false;
+        }
+    }
+}
